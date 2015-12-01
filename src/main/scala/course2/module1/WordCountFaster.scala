7e665b0de14f02bbe14ab5f94966805e9ef61f1d
Dean Wampler <dean@concurrentthought.com>
2015-11-30 19:23:28 -0600

    Two versions of Word Count; the original is "faster" and the new WordCount.scala is a less efficient "groupBy" implementation, which better matches the stages diagrams.

--- src/main/scala/course2/module1/WordCountFaster.scala
+++ src/main/scala/course2/module1/WordCountFaster.scala
@@ -1,20 +1,27 @@
 package course2.module1
 
 import org.apache.spark.SparkContext
+import util.Files
 
-
-object WordCountExample {
+/**
+ * Count the words in a corpus of documents.
+ * Uses the more efficient `reduceByKey` instead of `groupBy`.
+ */
+object WordCountFaster {
 
   def main(args: Array[String]): Unit = {
+    val inpath  = "data/all-shakespeare.txt"
+    val outpath = "output/word_count2"
+    Files.rmrf(outpath)  // delete old output (DON'T DO THIS IN PRODUCTION!)
+
     val sc = new SparkContext("local[*]", "Word Count")
     try {
-      val input = sc.textFile("data/all-shakespeare.txt").map(_.toLowerCase)
-      val wc = input
+      val wc = sc.textFile(inpath)
+        .map(_.toLowerCase)
         .flatMap(text => text.split("""\W+"""))
         .map(word => (word, 1))  // RDD[(String,Int)]
         .reduceByKey((n1, n2) => n1 + n2)   // or .reduceByKey(_ + _)
 
-      val outpath = s"output/wc-${System.currentTimeMillis()}"
       println(s"Writing output to: $outpath")
       wc.saveAsTextFile(outpath)
       printMsg("Enter any key to finish the job...")
